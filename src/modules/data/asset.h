db16f312fbe090c46baadccdaa1908bca6837e97
Dummiesman <aaro4130@gmail.com>
2021-09-03 17:22:36 -0400

    More datAssetManager stuff

--- src/modules/data/asset.h
+++ src/modules/data/asset.h
@@ -10,46 +10,31 @@ namespace MM2
     extern class Stream;
 
     // Class definitions
-
-    namespace $
-    {
-        namespace datAssetManager
-        {
-            declhook(0x4C55E0, _Func<void>, FullPath$1);
-            declhook(0x4C56F0, _Func<void>, FullPath$2);
-            declhook(0x4C59B0, _Func<bool>, Exists$1);
-            declhook(0x4C59E0, _Func<bool>, Exists$2);
-        }
-    }
-
     class datAssetManager {
     public:
-        /* TODO?
-        static Stream * Open(char const *,char const *,bool,bool);
-        static Stream * Open(char const *,char const *,char const *,bool,bool);
-        static Stream * Create(char const *,char const *,bool);
-        static Stream * Create(char const *,char const *,char const *,bool);
-        */
         static hook::Type<char *> sm_Path;
 
-        AGE_API static void FullPath(char *buffer, int length, LPCSTR directory, LPCSTR filename) {
-            $::datAssetManager::FullPath$1(buffer, length, directory, filename);
-        };
+        AGE_API static Stream * Open(LPCSTR directory, LPCSTR filename, bool foo, bool readonly)
+                                                                        { return hook::StaticThunk<0x4C5870>::Call<Stream *>(directory, filename, foo, readonly); }
+        AGE_API static Stream * Open(LPCSTR directory, LPCSTR filename, LPCSTR extension, bool foo, bool readonly)
+                                                                        { return hook::StaticThunk<0x4C58C0>::Call<Stream *>(directory, filename, extension, foo, readonly); }
 
-        AGE_API static void FullPath(char *buffer, int length, LPCSTR directory, LPCSTR filename, LPCSTR extension) {
-            $::datAssetManager::FullPath$2(buffer, length, directory, filename, extension);
-        };
+        AGE_API static void FullPath(char *buffer, int length, LPCSTR directory, LPCSTR filename)
+                                                                        { hook::StaticThunk<0x4C55E0>::Call<void>(buffer, length, directory, filename); }
+
+        AGE_API static void FullPath(char *buffer, int length, LPCSTR directory, LPCSTR filename, LPCSTR extension) 
+                                                                        { hook::StaticThunk<0x4C56F0>::Call<void>(buffer, length, directory, filename, extension); }
+        
 
         // these don't work for files outside of archives
-        AGE_API static bool Exists(LPCSTR directory, LPCSTR filename) {
-            return $::datAssetManager::Exists$1(directory, filename);
-        };
-
-        AGE_API static bool Exists(LPCSTR directory, LPCSTR filename, LPCSTR extension) {
-            return $::datAssetManager::Exists$2(directory, filename, extension);
-        };
-    };
+        AGE_API static bool Exists(LPCSTR directory, LPCSTR filename) 
+                                                                        { return hook::StaticThunk<0x4C59B0>::Call<bool>(directory, filename); }
+        
+        AGE_API static bool Exists(LPCSTR directory, LPCSTR filename, LPCSTR extension) 
+                                                                        { return hook::StaticThunk<0x4C59E0>::Call<bool>(directory, filename, extension); }
+        
 
     // Lua initialization
 
+    };
 }
