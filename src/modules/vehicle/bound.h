a80984782590b42e782a3c31b488c76673062ac7
Dummiesman <aaro4130@gmail.com>
2021-09-05 16:37:34 -0400

    Add vehBound

--- src/modules/vehicle/bound.h
+++ src/modules/vehicle/bound.h
@@ -1,15 +1,29 @@
 #pragma once
-#include <modules\vehicle.h>
+#include <modules\phys\phboundgeom.h>
 
 namespace MM2
 {
     // Forward declarations
-
+    class vehBound;
 
     // External declarations
-
+    extern class lvlMaterial;
 
     // Class definitions
+    class vehBound : public phBoundGeometry
+    {
+    private:
+        lvlMaterial* Material;
+    public:
+
+        AGE_API void Init()                                    { hook::Thunk<0x4DA6E0>::Call<void>(this); }
+
+        /*
+          phBound Overrides
+        */
+        AGE_API virtual void SetFriction(float a1)  override    { hook::Thunk<0x4DA700>::Call<void>(this, a1); }
+        AGE_API virtual void SetElasticity(float a1)  override  { hook::Thunk<0x4DA710>::Call<void>(this, a1); }
+    };
 
 
     // Lua initialization
