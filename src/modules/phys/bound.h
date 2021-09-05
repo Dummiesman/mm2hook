7b37e1e29b5a024c8e4a5c7a674c5c317b966804
Dummiesman <aaro4130@gmail.com>
2021-09-05 16:30:40 -0400

    Add all dg bound types

--- src/modules/phys/bound.h
+++ src/modules/phys/bound.h
@@ -4,13 +4,38 @@
 namespace MM2
 {
     // Forward declarations
-
+    class dgBoundBox;
+    class dgBoundHotdog;
+    class dgBoundSphere;
+    class dgBoundGeometry;
 
     // External declarations
-
+    extern class lvlMaterial;
 
     // Class definitions
+    class dgBoundBox : public phBoundBox
+    {
+        lvlMaterial* Material;
+    };
+    ASSERT_SIZEOF(dgBoundBox, 0x318);
 
+    class dgBoundHotdog : public phBoundHotdog
+    {
+        lvlMaterial* Material;
+    };
+    ASSERT_SIZEOF(dgBoundHotdog, 0x88);
+
+    class dgBoundSphere : public phBoundSphere
+    {
+        lvlMaterial* Material;
+    };
+    ASSERT_SIZEOF(dgBoundSphere, 0x84);
+
+    class dgBoundGeometry : public phBoundGeometry
+    {
+        lvlMaterial* Material;
+    };
+    ASSERT_SIZEOF(dgBoundGeometry, 0x80);
 
     // Lua initialization
 
