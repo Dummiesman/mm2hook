3444e904c1aec124f4510495e13ace916d54152a
Dummiesman <aaro4130@gmail.com>
2021-09-11 20:28:58 -0400

    Add LEVEL_ALLOCATOR

--- src/mm2_common.h
+++ src/mm2_common.h
@@ -25,9 +25,12 @@ Multiple declarations will cause compiler errors!
 // Allocator
 // Calls MM2's internal operator new
 //
-#define ANGEL_ALLOCATOR void* operator new(size_t size)          { Warningf("memAllocator allocation from hook"); return hook::StaticThunk<0x577360>::Call<void*>(size); } \
-                        void operator delete(void * pointer)     { Warningf("memAllocator delete from hook"); hook::StaticThunk<0x577380>::Call<void>(pointer);}       \
-                        void operator delete[](void * pointer)   { Warningf("memAllocator delete[] from hook"); hook::StaticThunk<0x5773C0>::Call<void>(pointer);}
+#define ANGEL_ALLOCATOR void* operator new(size_t size)          { return hook::StaticThunk<0x577360>::Call<void*>(size); } \
+                        void operator delete(void * pointer)     { hook::StaticThunk<0x577380>::Call<void>(pointer);}       \
+                        void operator delete[](void * pointer)   { hook::StaticThunk<0x5773C0>::Call<void>(pointer);}
+
+#define LEVEL_ALLOCATOR void* operator new(size_t size)          { return hook::StaticThunk<0x463110>::Call<void*>(size); } \
+                        void operator delete(void * pointer)     { hook::StaticThunk<0x463170>::Call<void>(pointer);}       
 //
 // MM2 uses DirectX 7
 //
