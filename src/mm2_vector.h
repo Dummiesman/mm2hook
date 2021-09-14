7779dce825c690fc439672986b0f85276c05a5b2
Dummiesman <aaro4130@gmail.com>
2021-09-14 11:19:47 -0400

    Move random functions into vector since they come from vector:randmath.obj

--- src/mm2_vector.h
+++ src/mm2_vector.h
@@ -10,6 +10,11 @@ namespace MM2
     class Vector3;
     class Vector4;
 
+    const auto DisableGlobalSeed = (void(*)(void))(0x4BBD50);
+    const auto EnableGlobalSeed = (void(*)(void))(0x4BBD80);
+    const auto frand = (float(*)(void))(0x4BBE30);
+    const auto irand = (int(*)(void))(0x4BBDF0);
+
     namespace $
     {
         namespace Matrix34 {
