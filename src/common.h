28158e7ef3c49b9f46091bbe78f614a11ef9cc03
Dummiesman <aaro4130@gmail.com>
2021-10-12 14:37:41 -0400

    Luaintf - enable vector and map types

--- src/common.h
+++ src/common.h
@@ -15,6 +15,9 @@
 #include <Shlwapi.h>
 #include <ShlObj.h>
 
+#include <vector>
+#include <map>
+
 /*
     Yeah because it's not like GetClassName would ever be used anywhere...*stares at Microsoft*
 */
@@ -33,9 +36,11 @@
 // Lua 5.3.2 embedded
 # include <lua/lua.hpp>
 # ifndef NO_LUAINTF
-# define LUAINTF_AUTO_DOWNCAST 0
+#  define LUAINTF_AUTO_DOWNCAST 0
 #  include <LuaIntf/LuaIntf.h>
 using namespace LuaIntf;
+LUA_USING_LIST_TYPE(std::vector)
+LUA_USING_MAP_TYPE(std::map)
 # endif // !NO_LUAINTF
 #endif // !_LUA_EMBED
 
