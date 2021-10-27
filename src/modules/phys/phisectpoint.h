0608f862a010d5b04ffb6d7fb0cff5bc36ff6d03
Dummiesman <aaro4130@gmail.com>
2021-10-27 03:18:33 -0400

    Add phIntersectionPoint Set

--- src/modules/phys/phisectpoint.h
+++ src/modules/phys/phisectpoint.h
@@ -12,11 +12,21 @@ namespace MM2
     // Class definitions
 
     struct phIntersectionPoint {
+    public:
         Vector3 Point;
         Vector3 Normal;
         float NormalizedDistance;
         float Penetration;
         byte IntersectResult;
+    public:
+        void Set(const Vector3& point, const Vector3& normal, float normalizedDistance, float penetration, bool result)
+        {
+            this->Point = point;
+            this->Normal = normal;
+            this->NormalizedDistance = normalizedDistance;
+            this->Penetration = penetration;
+            this->IntersectResult = result;
+        }
     };
     ASSERT_SIZEOF(phIntersectionPoint, 0x24);
 
