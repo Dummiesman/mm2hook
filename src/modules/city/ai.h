8c9300098dada61832d80a9f1b0240570c168099
Dummiesman <aaro4130@gmail.com>
2022-07-14 23:20:11 -0400

    Add lvlAiMap Draw from sdlview (untested)

--- src/modules/city/ai.h
+++ src/modules/city/ai.h
@@ -37,6 +37,84 @@ namespace MM2
         AGE_API static int GetNumVertexs() { return hook::StaticThunk<0x45DA60>::Call<int>(); }
         AGE_API static void GetStopLightPos(int a1, Vector3& a2, Vector3& a3, bool a4) { hook::StaticThunk<0x45E980>::Call<void>(a1, &a2, &a3, a4); }
 
+        AGE_API static void Draw(lvlSDL const & sdl) {
+            for (int i = 0; i < GetNumRoads(); i++) {
+                SetRoad(sdl, i, true);
+                int numVertices = GetNumVertexs();
+
+                for (int s = 0; s < 2; s++) {
+                    int numLanes = GetNumLanes(s);
+                    int intersectionType = GetIntersectionType(s);
+                    if (numLanes == 0) continue;
+
+                    vglBegin(gfxDrawMode::DRAWMODE_LINESTRIP, 0);
+                    vglCurrentColor.set(0xFFFFFFFF);
+
+                    Vector3 vertex = GetVertex(numVertices - 1, 0, s);
+                    vglVertex3f(vertex.X, vertex.Y + 0.15f, vertex.Z);
+
+                    Vector3 lightA, lightB;
+                    GetStopLightPos(1 - s, lightA, lightB, true);
+                    if (GetIntersectionType(1 - s) == 1)
+                    {
+                        vglCurrentColor.set(0xFFFF0000);
+                    }
+                    else if (GetIntersectionType(1 - s))
+                    {
+                        vglCurrentColor.set(0xFF0000FF);
+                    }
+                    else
+                    {
+                        vglCurrentColor.set(0xFF00FF00);
+                    }
+                    vglVertex3f(lightA.X, lightA.Y + 0.15f, lightA.Z);
+                    vglVertex3f(lightB.X, lightB.Y + 0.15f, lightB.Z);
+                    vglEnd();
+
+                    // draw light triangle
+                    Vector3 lightDir = (lightA - lightB);
+                    lightDir.Normalize();
+
+                    Vector3 midpoint = lightDir;
+                    midpoint.Scale(5.0f);
+                    midpoint += lightB;
+
+                    Vector3 endpoint = lightDir;
+                    endpoint.Cross(Vector3::YAXIS);
+                    endpoint.Scale(2.5f);
+                    endpoint += lightB;
+
+                    vglBegin(gfxDrawMode::DRAWMODE_TRIANGLELIST, 0);
+                    vglVertex3f(lightA.X, lightA.Y + 0.15f, lightA.Z);
+                    vglVertex3f(midpoint.X, midpoint.Y + 0.15f, midpoint.Z);
+                    vglVertex3f(endpoint.X, endpoint.Y + 0.15f, endpoint.Z);
+                    vglEnd();
+                }
+
+                for (int s = 0; s < 2; s++) {
+                    int numSubwayLanes = GetNumSubwayLanes(s);
+                    if (numSubwayLanes == 0) continue;
+
+                    for (int j = 0; j < numSubwayLanes; j++) {
+                        vglBegin(gfxDrawMode::DRAWMODE_LINESTRIP, 0);
+
+                        for (int k = 0; k < numVertices; k++) {
+                            float percentage = (float)k / (float)(numVertices - 1);
+                            int colorAmount = (int)(percentage * 255.0f);
+                            int invColorAmount = 255 - colorAmount;
+
+                            vglCurrentColor.set(invColorAmount | ((colorAmount | 0xFFFFFF00) << 16));
+
+                            Vector3 vertex = GetSubwayVertex(k, j, s);
+                            vglVertex3f(vertex.X, vertex.Y + 0.15f, vertex.Z);
+                        }
+
+                        vglEnd();
+                    }
+                }
+            }
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<lvlAiMap>("lvlAiMap")
                 //.addStaticFunction("SetRoad", static_cast<void(lvlAiMap::*)(lvlSDL const &, int, bool)>(&lvlAiMap::SetRoad))
