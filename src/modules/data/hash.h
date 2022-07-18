b71ac97900b9b9a7feda45bcf80280e04fcbe0eb
Dummiesman <aaro4130@gmail.com>
2022-07-18 08:44:42 -0400

    Add more HashTable functions, add ASSERT_SIZEOF

--- src/modules/data/hash.h
+++ src/modules/data/hash.h
@@ -12,11 +12,19 @@ namespace MM2
     // Class definitions
     class HashTable 
     {
+    private:
+        byte _buffer[0x18];
     public:
-        AGE_API void* Access(char const* a1)                            { return hook::Thunk<0x4C6CA0>::Call<void*>(this, a1); }
-        AGE_API bool Access(char const* a1, void* out)                  { return hook::Thunk<0x4C6D10>::Call<bool>(this, a1, out); }                        
-        AGE_API bool Insert(char const* a1, void* data)                 { return hook::Thunk<0x4C69F0>::Call<bool>(this, a1, data); }
+        AGE_API void* Access(char const* key)                            { return hook::Thunk<0x4C6CA0>::Call<void*>(this, key); }
+        AGE_API bool Access(char const* key, void* out)                  { return hook::Thunk<0x4C6D10>::Call<bool>(this, key, out); }                        
+        AGE_API bool Insert(char const* key, void* data)                 { return hook::Thunk<0x4C69F0>::Call<bool>(this, key, data); }
+        AGE_API char const* AccessName(int entryNum)                     { return hook::Thunk<0x4C6D80>::Call<char const*>(this, entryNum); }
+        AGE_API void* AccessData(int entryNum)                           { return hook::Thunk<0x4C6DA0>::Call<void*>(this, entryNum); }
+        AGE_API bool Change(char const* oldKey, char const* newKey)      { return hook::Thunk<0x4C6C60>::Call<bool>(this, oldKey, newKey); }
+        AGE_API bool Delete(char const* key)                             { return hook::Thunk<0x4C6B20>::Call<bool>(this, key); }
     };
+
+    ASSERT_SIZEOF(HashTable, 0x18);
 
     // Lua initialization
 
