ff753f9ad5167fb2992a56d64122503ac7399b32
Dummiesman <aaro4130@gmail.com>
2022-07-19 16:39:29 -0400

    implement police car audio Lua

--- src/modules/vehicle/policecaraudio.h
+++ src/modules/vehicle/policecaraudio.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <modules\vehicle.h>
+#include "caraudio.h"
 
 namespace MM2
 {
@@ -11,15 +11,20 @@ namespace MM2
 
     // Class definitions
 
-    class vehPoliceCarAudio {
+    class vehPoliceCarAudio : public vehCarAudio {
     public:
         static hook::Type<int> iNumCopsPursuingPlayer;
 
         AGE_API void StartSiren(int a1)                   { hook::Thunk<0x4D4B20>::Call<void>(this, a1); }
         AGE_API void StopSiren()                          { hook::Thunk<0x4D4C20>::Call<void>(this); }
         AGE_API void PlayExplosion()                      { hook::Thunk<0x4D4C90>::Call<void>(this); }
+
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginExtendClass<vehPoliceCarAudio, vehCarAudio>("vehPoliceCarAudio")
+                .addFunction("StartSiren", &StartSiren)
+                .addFunction("StopSiren", &StopSiren)
+                .addFunction("PlayExplosion", &PlayExplosion)
+                .endClass();
+        }
     };
-
-    // Lua initialization
-
 }
