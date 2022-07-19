60e0355662de089c8a38577049cbd979b861dfbd
Dummiesman <aaro4130@gmail.com>
2022-07-19 16:39:41 -0400

    Map nitro car audio, implement Lua

--- src/modules/vehicle/nitrocaraudio.h
+++ src/modules/vehicle/nitrocaraudio.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <modules\vehicle.h>
+#include "caraudio.h"
 
 namespace MM2
 {
@@ -10,8 +10,36 @@ namespace MM2
 
 
     // Class definitions
+    class vehNitroCarAudio : public vehCarAudio
+    {
+    private:
+        float NitroVolume;
+        int NitroSampleID;
+        AudSoundBase* NitroSample;
+    public:
+        /*
+            Aud3DObject virtuals
+        */
+        AGE_API void AssignSounds() override                    { hook::Thunk<0x4DD390>::Call<void>(this); }
+        AGE_API void UnAssignSounds(int a1) override            { hook::Thunk<0x4DD400>::Call<void>(this, a1); }
+        AGE_API void UpdateAudio() override                     { hook::Thunk<0x4DD2A0>::Call<void>(this); }
+        AGE_API void Update() override                          { hook::Thunk<0x4DD5E0>::Call<void>(this); }
+        AGE_API void SetNon3DParams() override                  { hook::Thunk<0x4DD560>::Call<void>(this); }
+        AGE_API void Set3DParams()  override                    { hook::Thunk<0x4DD5D0>::Call<void>(this); }
 
+        /*
+            vehNitroCarAudio
+        */
+        AGE_API void PlayNitro()                                { hook::Thunk<0x4DD6D0>::Call<void>(this); }
+        AGE_API void StopNitro()                                { hook::Thunk<0x4DD760>::Call<void>(this); }
+        
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginExtendClass<vehNitroCarAudio, vehCarAudio>("vehNitroCarAudio")
+                .addFunction("PlayNitro", &PlayNitro)
+                .addFunction("StopNitro", &StopNitro)
+                .endClass();
+        }
+    };
 
-    // Lua initialization
-
+    ASSERT_SIZEOF(vehNitroCarAudio, 0x13C);
 }
