94dd5fb178b9a86d59c987a468f6bc731bb09420
Dummiesman <aaro4130@gmail.com>
2022-07-19 02:07:51 -0400

    Random constructor now calls Seed to prevent undefined state

--- src/modules/node/random.h
+++ src/modules/node/random.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <mm2_common.h>
+#include <time.h>
 
 namespace MM2
 {
@@ -16,6 +17,10 @@ namespace MM2
         byte Index2;
         int Array[56];
     public:
+        AGE_API Random() {
+            Seed(static_cast<int>(time(NULL)));
+        }
+
         AGE_API float Normal(float a1, float a2)                { return hook::Thunk<0x4A38E0>::Call<float>(this, a1, a2); }
         AGE_API float Number()                                  { return hook::Thunk<0x4A3880>::Call<float>(this); }
         AGE_API void Seed(int seed)                             { hook::Thunk<0x4A37D0>::Call<void>(this, seed); }
@@ -29,4 +34,6 @@ namespace MM2
                 .endClass();
         }
     };
+
+    ASSERT_SIZEOF(Random, 0xE4);
 }
