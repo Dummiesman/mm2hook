8415de4987e58cbe967b596f7701a495e95600c9
Dummiesman <aaro4130@gmail.com>
2022-07-19 16:39:15 -0400

    Map semi car audio, implement Lua

--- src/modules/vehicle/semicaraudio.h
+++ src/modules/vehicle/semicaraudio.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <modules\vehicle.h>
+#include "caraudio.h"
 
 namespace MM2
 {
@@ -10,8 +10,31 @@ namespace MM2
 
 
     // Class definitions
+    class vehSemiCarAudio : public vehCarAudio
+    {
+    private:
+        float ReverseVolume;
+        float AirBlowVolume;
+        AudSoundBase* ReverseAudio;
+        AudSoundBase* AirBlowAudio;
+        int AirBlowSampleIndex;
+        int ReverseSampleIndex;
+    public:
+        /*
+            Aud3DObject virtuals
+        */
+        AGE_API void AssignSounds() override                    { hook::Thunk<0x4DCC50>::Call<void>(this); }
+        AGE_API void UnAssignSounds(int a1) override            { hook::Thunk<0x4DCCF0>::Call<void>(this, a1); }
+        AGE_API void UpdateAudio() override                     { hook::Thunk<0x4DCA40>::Call<void>(this); }
+        AGE_API void Update() override                          { hook::Thunk<0x4DCF40>::Call<void>(this); }
+        AGE_API void SetNon3DParams() override                  { hook::Thunk<0x4DCE90>::Call<void>(this); }
+        AGE_API void Set3DParams()  override                    { hook::Thunk<0x4DCF30>::Call<void>(this); }
 
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginExtendClass<vehSemiCarAudio, vehCarAudio>("vehSemiCarAudio")
+                .endClass();
+        }
+    };
 
-    // Lua initialization
-
+    ASSERT_SIZEOF(vehSemiCarAudio, 0x148);
 }
