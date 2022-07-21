e72def4f14a531bec799c98bf2f8aa522b4e621e
Dummiesman <aaro4130@gmail.com>
2022-07-21 05:37:46 -0400

    Replace ptrs in aiPath parameters with references, make functions const

--- src/modules/ai/aiPath.h
+++ src/modules/ai/aiPath.h
@@ -17,7 +17,7 @@ namespace MM2
         hook::Field<0x18, float> _baseSpeedLimit;
     private:
         //lua helper
-        std::tuple<int, float> luaIsPosOnRoad(Vector3* pos, float margin)
+        std::tuple<int, float> luaIsPosOnRoad(Vector3 const & pos, float margin) const
         {
             float outVal = 0.f;
             int posOnRoadVal = this->IsPosOnRoad(pos, margin, &outVal);
@@ -38,11 +38,11 @@ namespace MM2
         */
         AGE_API void ClearAmbients()                        { hook::Thunk<0x547C00>::Call<void>(this); }
         AGE_API void ClearPeds()                            { hook::Thunk<0x547BD0>::Call<void>(this); }
-        AGE_API bool HasCableCarLine(int roadSide)          { return hook::Thunk<0x549980>::Call<bool>(this, roadSide); }
-        AGE_API bool HasSubwayLine(int roadSide)            { return hook::Thunk<0x5499B0>::Call<bool>(this, roadSide); }
-        AGE_API int IsPosOnRoad(Vector3* pos, float margin, float* outDistanceFromCenter)
-                                                            { return hook::Thunk<0x548370>::Call<int>(this, pos, margin, outDistanceFromCenter); }
-        AGE_API int Lane(Vector3 *pos, int roadSide)        { return hook::Thunk<0x547900>::Call<int>(this, pos, roadSide); }
+        AGE_API bool HasCableCarLine(int roadSide) const    { return hook::Thunk<0x549980>::Call<bool>(this, roadSide); }
+        AGE_API bool HasSubwayLine(int roadSide) const      { return hook::Thunk<0x5499B0>::Call<bool>(this, roadSide); }
+        AGE_API int IsPosOnRoad(Vector3 const & pos, float margin, float* outDistanceFromCenter) const
+                                                            { return hook::Thunk<0x548370>::Call<int>(this, &pos, margin, outDistanceFromCenter); }
+        AGE_API int Lane(Vector3 & pos, int roadSide) const { return hook::Thunk<0x547900>::Call<int>(this, &pos, roadSide); }
         AGE_API void UpdatePedestrians()                    { hook::Thunk<0x544150>::Call<void>(this); }
 
         static void BindLua(LuaState L) {
