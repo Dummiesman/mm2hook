8073b75db28c6aa47bc373c98e3f4c8f02f6838c
Dummiesman <aaro4130@gmail.com>
2022-07-27 12:58:47 -0400

    Actually lets mimic hook config api

--- src/modules/data/args.cpp
+++ src/modules/data/args.cpp
@@ -6,30 +6,19 @@ namespace MM2
     declfield(datArgParser::Argc)(0x6A3C28);
     declfield(datArgParser::Argv)(0x6A3C30);
 
-    template<class outType>
-    std::tuple<bool, outType> datArgParser::getTyped(LPCSTR arg, UINT index, bool(*func)(LPCSTR, UINT, outType*), outType defaultValue)
-    {
-        outType out;
-        bool res = func(arg, index, &out);
-        if(res)
-            return std::make_tuple<bool, outType>(std::move(res), std::move(out));
-        else
-            return std::make_tuple<bool, outType>(std::move(res), defaultValue);
+    int datArgParser::getIntLua(LPCSTR arg, UINT index, int defaultValue) {
+        int out;
+        return datArgParser::Get(arg, index, &out) ? out : defaultValue;
     }
 
-    std::tuple<bool, int> datArgParser::getInt(LPCSTR arg, int index)
-    {
-        return getTyped<int>(arg, index, Get, 0);
+    float datArgParser::getFloatLua(LPCSTR arg, UINT index, float defaultValue) {
+        float out;
+        return datArgParser::Get(arg, index, &out) ? out : defaultValue;
     }
 
-    std::tuple<bool, float> datArgParser::getFloat(LPCSTR arg, int index)
-    {
-        return getTyped<float>(arg, index, Get, 0.f);
-    }
-
-    std::tuple<bool, LPCSTR> datArgParser::getString(LPCSTR arg, int index)
-    {
-        return getTyped<LPCSTR>(arg, index, Get, nullptr);
+    LPCSTR datArgParser::getStringLua(LPCSTR arg, UINT index, LPCSTR defaultValue) {
+        LPCSTR out;
+        return datArgParser::Get(arg, index, &out) ? out : defaultValue;
     }
 
     AGE_API bool datArgParser::Get(LPCSTR arg)                          { return hook::StaticThunk<0x4C6190>::Call<bool>(arg); }
@@ -42,9 +31,9 @@ namespace MM2
         LuaBinding(L).beginClass<datArgParser>("datArgParser")
             .addStaticFunction("Get", static_cast<bool (*)(LPCSTR)>(&Get))
             .addStaticFunction("GetNum", &GetNum)
-            .addStaticFunction("GetInt", &getInt, LUA_ARGS(LPCSTR, _def<int, 0>))
-            .addStaticFunction("GetFloat", &getFloat, LUA_ARGS(LPCSTR, _def<int, 0>))
-            .addStaticFunction("GetString", &getString, LUA_ARGS(LPCSTR, _def<int, 0>))
+            .addStaticFunction("GetInt", &getIntLua)
+            .addStaticFunction("GetFloat", &getFloatLua)
+            .addStaticFunction("GetString", &getStringLua)
             .endClass();
     }
 }
--- src/modules/data/args.h
+++ src/modules/data/args.h
@@ -13,12 +13,9 @@ namespace MM2
     class datArgParser {
     private:
         //lua helpers
-        template<class outType>
-        static std::tuple<bool, outType> getTyped(LPCSTR arg, UINT index, bool(*func)(LPCSTR, UINT, outType*), outType defaultValue);
-
-        static std::tuple<bool, int> getInt(LPCSTR arg, int index = 0);
-        static std::tuple<bool, float> getFloat(LPCSTR arg, int index = 0);
-        static std::tuple<bool, LPCSTR> getString(LPCSTR arg, int index = 0);
+        static int getIntLua(LPCSTR arg, UINT index, int defaultValue);
+        static float getFloatLua(LPCSTR arg, UINT index, float defaultValue);
+        static LPCSTR getStringLua(LPCSTR arg, UINT index, LPCSTR defaultValue);
     public:
         static hook::Type<int> Argc;
         static hook::Type<char **> Argv;
