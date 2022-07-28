13acaa4931c3db66bff1fb094f49e1c7801c55cc
Dummiesman <aaro4130@gmail.com>
2022-07-28 04:00:36 -0400

    Name AudCreatureContainer PlayImpactReaction param also add ASSERT SIZEOF

--- src/modules/audio/audcreaturecontainer.h
+++ src/modules/audio/audcreaturecontainer.h
@@ -11,8 +11,12 @@ namespace MM2
 
     // Class definitions
     class AudCreatureContainer : public Aud3DObject {
+    private:
+        byte _buffer[0x14];
     public:
-        AGE_API void PlayImpactReaction(float a1) { hook::Thunk<0x510CC0>::Call<void>(this, a1); }
+        AGE_API void PlayImpactReaction(float force) { hook::Thunk<0x510CC0>::Call<void>(this, force); }
         AGE_API void PlayAvoidanceReaction(float a1) { hook::Thunk<0x510CF0>::Call<void>(this, a1); }
     };
+
+    ASSERT_SIZEOF(AudCreatureContainer, 0x74);
 }
