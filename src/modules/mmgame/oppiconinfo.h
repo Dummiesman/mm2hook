df9d46c896912a01b0f087216ac71b02403874ba
Dummiesman <aaro4130@gmail.com>
2022-11-06 19:36:06 -0500

    On second thought, OppIconInfo Get/Set Name/Color shouldn't be Lua only

--- src/modules/mmgame/oppiconinfo.h
+++ src/modules/mmgame/oppiconinfo.h
@@ -12,29 +12,6 @@ namespace MM2
 
     // Class definitions
 	struct OppIconInfo {
-    private:
-        // lua helpers
-        LPCSTR luaGetText() const
-        {
-            return Text;
-        }
-
-        void luaSetText(LPCSTR text)
-        {
-            strncpy_s(Text, text, sizeof(Text));
-        }
-
-        Vector4 luaGetColor() const
-        {
-            Vector4 vec;
-            vec.UnpackColorBGRA(this->Color);
-            return vec;
-        }
-
-        void luaSetColor(const Vector4& color)
-        {
-            this->Color = color.PackColorBGRA();
-        }
     public:
         unsigned int Color;
         bool Enabled;
@@ -44,12 +21,34 @@ namespace MM2
         gfxBitmap* Bitmap;
         float Scale;
     public:
+        LPCSTR GetText() const
+        {
+            return Text;
+        }
+
+        void SetText(LPCSTR text)
+        {
+            strncpy_s(Text, text, sizeof(Text));
+        }
+
+        Vector4 GetColor() const
+        {
+            Vector4 vec;
+            vec.UnpackColorBGRA(this->Color);
+            return vec;
+        }
+
+        void SetColor(const Vector4& color)
+        {
+            this->Color = color.PackColorBGRA();
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<OppIconInfo>("OppIconInfo")
-                .addProperty("Color", &luaGetColor, &luaSetColor)
+                .addProperty("Color", &GetColor, &SetColor)
                 .addVariable("Enabled", &OppIconInfo::Enabled)
                 .addVariable("IconIndex", &OppIconInfo::IconIndex)
-                .addProperty("Text", &luaGetText, &luaSetText)
+                .addProperty("Text", &GetText, &SetText)
                 .addVariable("Bitmap", &OppIconInfo::Bitmap)
                 .addVariable("Scale", &OppIconInfo::Scale)
                 .endClass();
