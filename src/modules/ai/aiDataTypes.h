fcbd44c6c71241478735d8d09240644db60c3e5c
Dummiesman <aaro4130@gmail.com>
2023-06-09 14:24:13 -0400

    Add Lua stuff for aiDataTypes

--- src/modules/ai/aiDataTypes.h
+++ src/modules/ai/aiDataTypes.h
@@ -20,8 +20,14 @@ namespace MM2
         float SpawnChance;
         int dword_24;
 
+        LPCSTR GetName() {
+            return Name;
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<aiAmbientTypeData>("aiAmbientTypeData")
+                .addPropertyReadOnly("Name", &GetName)
+                .addVariable("SpawnChance", &aiAmbientTypeData::SpawnChance, false)
                 .endClass();
         }
     };
@@ -34,6 +40,9 @@ namespace MM2
 
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<aiExceptionData>("aiExceptionData")
+                .addVariable("RoadId", &aiExceptionData::RoadId, false)
+                .addVariable("Density", &aiExceptionData::Density, false)
+                .addVariable("SpeedLimit", &aiExceptionData::SpeedLimit, false)
                 .endClass();
         }
     };
@@ -46,8 +55,19 @@ namespace MM2
         float float_4c;
         char dword_50;
 
+        LPCSTR GetBasename() {
+            return Basename;
+        }
+
+        LPCSTR GetWaypointFileName() {
+            return WaypointFileName;
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<HookmanData>("HookmanData")
+                .addPropertyReadOnly("Basename", &GetBasename)
+                .addPropertyReadOnly("WaypointFileName", &GetWaypointFileName)
+                .addVariable("HideoutPosition", &HookmanData::HideoutPosition, false)
                 .endClass();
         }
     };
@@ -56,10 +76,10 @@ namespace MM2
     {
         char Basename[32];
         char WaypointFileName[32];
-        float SpeedMultiplier;
-        float CornerCautionThreshold;
-        float CornerCaution;
-        float BrakingBias;
+        float MaxThrottle;
+        float TurnBrakingThreshold;
+        float TurnRadius;
+        float TurnSpeedMultiplier;
         char UnusedFlag;
         char AvoidTraffic;
         char AvoidProps;
@@ -67,8 +87,26 @@ namespace MM2
         char AvoidOpponents;
         char BadPathfinding;
 
+        LPCSTR GetBasename() {
+            return Basename;
+        }
+
+        LPCSTR GetWaypointFileName() {
+            return WaypointFileName;
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<OpponentData>("OpponentData")
+                .addPropertyReadOnly("Basename", &GetBasename)
+                .addPropertyReadOnly("WaypointFileName", &GetWaypointFileName)
+                .addVariable("MaxThrottle", &OpponentData::MaxThrottle, false)
+                .addVariable("TurnBrakingThreshold", &OpponentData::TurnBrakingThreshold, false)
+                .addVariable("TurnRadius", &OpponentData::TurnRadius, false)
+                .addVariable("TurnSpeedMultiplier", &OpponentData::TurnSpeedMultiplier, false)
+                .addVariable("AvoidTraffic", &OpponentData::AvoidTraffic, false)
+                .addVariable("AvoidProps", &OpponentData::AvoidProps, false)
+                .addVariable("AvoidPlayers", &OpponentData::AvoidPlayers, false)
+                .addVariable("AvoidOpponents", &OpponentData::AvoidOpponents, false)
                 .endClass();
         }
     };
@@ -83,8 +121,17 @@ namespace MM2
         int Unk1;
         int Flags;
 
+        LPCSTR GetBasename() {
+            return Basename;
+        }
+
         static void BindLua(LuaState L) {
             LuaBinding(L).beginClass<PoliceData>("PoliceData")
+                .addPropertyReadOnly("Basename", &GetBasename)
+                .addVariable("Position", &PoliceData::Position, false)
+                .addVariable("Rotation", &PoliceData::Rotation, false)
+                .addVariable("ChaseDistance", &PoliceData::ChaseDistance, false)
+                .addVariable("Flags", &PoliceData::Flags, false)
                 .endClass();
         }
     };
