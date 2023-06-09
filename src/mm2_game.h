35fb2007c34c19a4be664f2d80a412756ac7ab2a
Dummiesman <aaro4130@gmail.com>
2023-06-09 13:48:52 -0400

    Add mmWaypoints

--- src/mm2_game.h
+++ src/mm2_game.h
@@ -18,6 +18,7 @@ namespace MM2
         luaBind<mmNumber>(L);
         luaBind<mmTextNode>(L);
         luaBind<mmMirror>(L);
+        luaBind<mmWaypoints>(L);
         luaBind<mmGameHUD>(L);
         luaBind<mmCRHUD>(L);
         luaBind<mmSpeedIndicator>(L);
---
+++ src/modules/mmgame/waypoints.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <modules\node.h>
+
+namespace MM2
+{
+    // Forward declarations
+    class mmWaypoints;
+
+    // External declarations
+
+    // Class definitions
+	class mmWaypoints : public asNode
+	{
+	private:
+		char _buffer[0x78];
+	private:
+		Vector3 getWaypointLua(int index)
+		{
+			Vector3 out;
+			GetWaypoint(index, out);
+			return out;
+		}
+
+		Vector3 getStartLua()
+		{
+			Vector3 out;
+			GetStart(out);
+			return out;
+		}
+	protected:
+		static hook::Field<0x2C, int> _currentGoal;
+		static hook::Field<0x30, int> _waypointCount;
+		static hook::Field<0x50, int> _numCleared;
+	public:
+        /*
+            asNode virtuals
+        */
+
+		virtual AGE_API void Cull(void) override					{ hook::Thunk<0x435240>::Call<void>(this); };
+        virtual AGE_API void Update(void) override					{ hook::Thunk<0x435280>::Call<void>(this); };
+        virtual AGE_API void Reset(void) override					{ hook::Thunk<0x4357E0>::Call<void>(this); };
+
+		/*
+			mmWaypoints
+		*/
+
+		AGE_API void DeactivateFinish()								{ hook::Thunk<0x4357D0>::Call<void>(this); }
+		AGE_API void GetWaypoint(int index, Vector3 & out) const	{ hook::Thunk<0x435930>::Call<void>(this, index, &out); }
+		AGE_API float GetHeading(int index) const					{ return hook::Thunk<0x435970>::Call<float>(this, index); }
+		AGE_API void GetStart(Vector3 & out) const					{ hook::Thunk<0x4358F0>::Call<void>(this, &out); }
+		AGE_API float GetStartAngle() const							{ return hook::Thunk<0x434960>::Call<float>(this); }
+		AGE_API int GetHitRoom(int index) const						{ return hook::Thunk<0x435A00>::Call<int>(this, index); }
+		AGE_API float GetCurrentDistance() const					{ return hook::Thunk<0x435A50>::Call<float>(this); }
+
+		int GetWaypointCount() const 
+		{
+			return _waypointCount.get(this);
+		}
+
+		int GetCurrentGoal() const
+		{
+			return _currentGoal.get(this);
+		}
+
+		int GetClearedWaypointCount() const
+		{
+			return _numCleared.get(this);
+		}
+		
+		static void BindLua(LuaState L) {
+			LuaBinding(L).beginExtendClass<mmWaypoints, asNode>("mmWaypoints")
+				.addPropertyReadOnly("NumWaypoints", &GetWaypointCount)
+				.addPropertyReadOnly("NumCleared", &GetClearedWaypointCount)
+				.addPropertyReadOnly("CurrentGoal", &GetCurrentGoal)
+				.addFunction("DeactivateFinish", &DeactivateFinish)
+				.addFunction("GetWaypoint", &getWaypointLua)
+				.addFunction("GetHeading", &GetHeading)
+				.addFunction("GetStart", &getStartLua)
+				.addFunction("GetStartAngle", &GetStartAngle)
+				.addFunction("GetHitRoom", &GetHitRoom)
+				.addFunction("GetCurrentDistance", &GetCurrentDistance)
+				.endClass();
+		}
+	};
+	ASSERT_SIZEOF(mmWaypoints, 0x90);
+
+}
