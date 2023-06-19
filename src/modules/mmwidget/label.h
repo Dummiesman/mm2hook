344b309c595cebee8eb7eef526f1f4c9efa377c1
Dummiesman <aaro4130@gmail.com>
2023-06-19 09:45:27 -0400

    Map out UILabel virtuals and luaBind it

--- src/modules/mmwidget/label.h
+++ src/modules/mmwidget/label.h
@@ -17,13 +17,35 @@ namespace MM2
         uint32_t Flags;
         uint32_t State;
         float ElapsedTime;
+    public:
 
+        /*
+            asNode virtuals
+        */
+
+        virtual AGE_API void Update(void) override          { hook::Thunk<0x4ED3C0>::Call<void>(this); };
+
+        /*
+            uiWidget virtuals
+        */
+
+        virtual AGE_API void Action(eqEvent event)          { hook::Thunk<0x4ED4C0>::Call<void>(this, event); }
+        virtual AGE_API void Switch(int a1)                 { hook::Thunk<0x4ED430>::Call<void>(this, a1); }
+        
+        /*
+            uiLabel members
+        */
         AGE_API void SetText(char* text) {
             hook::Thunk<0x4ED3A0>::Call<void>(this, text);
         }
 
         AGE_API void SetBlink(bool blink) {
             hook::Thunk<0x4ED400>::Call<void>(this, blink);
+        }
+
+        static void BindLua(LuaState L) {
+            LuaBinding(L).beginExtendClass<UILabel, uiWidget>("UILabel")
+                .endClass();
         }
     };
 }
