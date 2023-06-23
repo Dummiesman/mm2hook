c703d67af601c1b33df7cdb8a8cd26dad995930f
Dummiesman <aaro4130@gmail.com>
2023-06-22 20:33:43 -0400

    mmInterface class

---
+++ src/modules/mmgame/interface.cpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "interface.h"
+
+using namespace MM2;
+
+mmInterface* mmInterface::Instance = nullptr;
---
+++ src/modules/mmgame/interface.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <modules\node.h>
+
+namespace MM2
+{
+    // Forward declarations
+    class mmInterface;
+
+    // External declarations
+	extern class Vehicle;
+	extern class UIMenu;
+
+    // Class definitions
+	class mmInterface : public asNode
+	{
+	private:
+		char _buffer[0x7738];
+	protected:
+		static hook::Field<0x18, UIMenu*> _mainMenu;
+		static hook::Field<0x24, Vehicle*> _vehiclesMenu;
+
+		static mmInterface* Instance; // not in MM2 in actuality but since we're allowing things like custom menus
+									  // we'll define this
+	public:
+		static mmInterface* GetInstance()
+		{
+			return mmInterface::Instance;
+		}
+
+		/// <summary>
+		/// INTERNAL: Do not use
+		/// </summary>
+		static void ClearInstance()
+		{
+			mmInterface::Instance = nullptr;
+		}
+
+		/// <summary>
+		/// INTERNAL: Do not use
+		/// </summary>
+		static void SetInstance(mmInterface* instance)
+		{
+			mmInterface::Instance = instance;
+		}
+	public:
+
+        /*
+            asNode virtuals
+        */
+
+        virtual AGE_API void Update(void) override          { hook::Thunk<0x40A6C0>::Call<void>(this); };
+        virtual AGE_API void Reset(void) override           { hook::Thunk<0x40A130>::Call<void>(this); };
+
+		/*
+			mmInterface members
+		*/
+		AGE_API void BeDone()								{ hook::Thunk<0x40A5B0>::Call<void>(this); }
+
+		UIMenu* GetMainMenu()
+		{
+			return _mainMenu.get(this);
+		}
+
+		Vehicle* GetVehiclesMenu()
+		{
+			return _vehiclesMenu.get(this);
+		}
+
+		static void BindLua(LuaState L) {
+			LuaBinding(L).beginExtendClass<mmInterface, asNode>("mmInterface")
+				.addPropertyReadOnly("MainMenu", &GetMainMenu)
+				.addPropertyReadOnly("VehiclesMenu", &GetVehiclesMenu)
+				.addFunction("BeDone", &BeDone)
+				.endClass();
+		}
+	};
+	ASSERT_SIZEOF(mmInterface, 0x7750);
+}
