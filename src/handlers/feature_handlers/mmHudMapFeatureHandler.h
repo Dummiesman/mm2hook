a18d7616b63cad66806ee163d9aa2a101c7d106f
Dummiesman <aaro4130@gmail.com>
2023-06-30 11:32:01 -0400

    Use namespace mm2 in mmHudMapFeatureHandler header

--- src/handlers/feature_handlers/mmHudMapFeatureHandler.h
+++ src/handlers/feature_handlers/mmHudMapFeatureHandler.h
@@ -3,19 +3,23 @@
 #include "patch.h"
 #include "mm2.h"
 
+using namespace MM2;
+
 class mmHudMapFeatureHandler {
 public:
-    void DrawColoredTri(unsigned int color, const MM2::Matrix34& a2);
-    void DrawWhiteTri(const MM2::Matrix34& a1);
-    void DrawLightOrangeTri(const MM2::Matrix34& a1);
-    void DrawLightGreenTri(const MM2::Matrix34& a1);
+    void DrawColoredTri(unsigned int color, const Matrix34& a2);
+    void DrawWhiteTri(const Matrix34& a1);
+    void DrawLightOrangeTri(const Matrix34& a1);
+    void DrawLightGreenTri(const Matrix34& a1);
     
-    void DrawIcon(int iconType, const MM2::Matrix34& matrix);
-    void DrawOutlinedIcon(int iconType, const MM2::Matrix34& matrix);
-    void DrawOutlinedIcon(int iconType, int iconOutlineType, const MM2::Matrix34& matrix);
+    void DrawIcon(int iconType, const Matrix34& matrix);
+    void DrawOutlinedIcon(int iconType, const Matrix34& matrix);
+    void DrawOutlinedIcon(int iconType, int iconOutlineType, const Matrix34& matrix);
 
-    void DrawNfsMwPlayerIcon(const MM2::Matrix34& matrix);
-    void DrawNfsMwOpponentIcon(const MM2::Matrix34& matrix);
+    void DrawCustomIcon(int iconType, unsigned int color, const Matrix34& matrix);
+
+    void DrawNfsMwPlayerIcon(const Matrix34& matrix);
+    void DrawNfsMwOpponentIcon(const Matrix34& matrix);
     void DrawPlayer();
     void DrawCops();
     void DrawOpponents();
