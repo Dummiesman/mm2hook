214506027d8b1c5548481c1a4164f83a1203f77d
Dummiesman <aaro4130@gmail.com>
2023-10-01 18:58:43 -0400

    Add gfxPipeline declfield for earlier commit

---
+++ src/modules/gfx/pipeline.cpp
@@ -0,0 +1,8 @@
+#include "pipeline.h"
+
+namespace MM2
+{
+    declfield(gfxPipeline::m_Viewport)(0x683124);
+    declfield(gfxPipeline::VP)(0x6830FC);
+    declfield(gfxPipeline::OrthoVP)(0x68310C);
+}
