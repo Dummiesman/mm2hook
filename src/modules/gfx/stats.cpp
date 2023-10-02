fcad02830540f1a7ed3f6f06260466ab7739eb88
Dummiesman <aaro4130@gmail.com>
2023-10-02 11:01:21 -0400

    Add gfx stats for future use

---
+++ src/modules/gfx/stats.cpp
@@ -0,0 +1,9 @@
+#include "stats.h"
+
+int gfxLines = 0;
+int gfxStateChanges = 0;
+int gfxTris = 0;
+int gfxVerts = 0;
+float gfxFrameTime = 0;
+int gfxTextureChanges = 0;
+int gfxPoints = 0;
---
+++ src/modules/gfx/stats.h
@@ -0,0 +1,7 @@
+extern int gfxLines;
+extern int gfxStateChanges;
+extern int gfxTris;
+extern int gfxVerts;
+extern float gfxFrameTime;
+extern int gfxTextureChanges;
+extern int gfxPoints;
