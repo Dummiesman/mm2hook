56d545a379d455419c0e1cd8ab3346f1ae3463e8
Dummiesman <aaro4130@gmail.com>
2023-12-25 14:52:47 -0500

    Split aiVehicleSpline impl/header

---
+++ src/modules/ai/aiVehicleSpline.cpp
@@ -0,0 +1,76 @@
+#pragma once
+#include "aiVehicleSpline.h"
+#include <modules/rgl/vgl.h>
+
+using namespace MM2;
+
+
+void aiVehicleSpline::UpdateObstacleMap()               { hook::Thunk<0x568410>::Call<void>(this); }
+
+void aiVehicleSpline::Position(Vector3 &a1)             { hook::Thunk<0x551C40>::Call<void>(this, &a1); }
+float aiVehicleSpline::Speed()                          { return hook::Thunk<0x51C70>::Call<float>(this); }
+int aiVehicleSpline::CurrentRoadIdx(aiPath **a1, const bool *a2, int *a3)
+                                                        { return hook::Thunk<0x5681C0>::Call<int>(this, a1, a2, a3); }
+int aiVehicleSpline::CurrentRdVert()                    { return hook::Thunk<0x5683F0>::Call<int>(this); }
+
+void aiVehicleSpline::Update()                          FORWARD_THUNK;
+void aiVehicleSpline::Reset()                           FORWARD_THUNK;
+int aiVehicleSpline::Type()                             { return hook::Thunk<0x56A700>::Call<int>(this); }
+Matrix34 & aiVehicleSpline::GetMatrix() const           { return hook::Thunk<0x551C80>::Call<Matrix34&>(this); }
+float aiVehicleSpline::FrontBumperDistance()            { return hook::Thunk<0x551C00>::Call<float>(this); }
+float aiVehicleSpline::BackBumperDistance()             { return hook::Thunk<0x551C10>::Call<float>(this); }
+float aiVehicleSpline::LSideDistance()                  { return hook::Thunk<0x551C20>::Call<float>(this); }
+float aiVehicleSpline::RSideDistance()                  { return hook::Thunk<0x551C30>::Call<float>(this); }
+int aiVehicleSpline::CurrentLane()                      { return hook::Thunk<0x551BE0>::Call<int>(this); }
+int aiVehicleSpline::CurrentRoadId()                    { return hook::Thunk<0x51BF0>::Call<int>(this); }
+
+void aiVehicleSpline::DrawId() const
+{
+    Vector3 position = GetMatrix().GetRow(3);
+    position.Y += 4.4f;
+    vglDrawLabelf(position, "%d", this->GetId());
+}
+
+void aiVehicleSpline::Impact(int a1)                     FORWARD_THUNK;
+AudImpact* aiVehicleSpline::GetAudImpactPtr()            { return hook::Thunk<0x551C90>::Call<AudImpact*>(this); }
+void aiVehicleSpline::PlayHorn(float a1, float a2)       { hook::Thunk<0x551CA0>::Call<void>(this, a1, a2); }
+void aiVehicleSpline::StopVoice()                        { hook::Thunk<0x551CB0>::Call<void>(this); }
+
+//fields
+float aiVehicleSpline::GetSpeed() const
+{
+    return _curSpeed.get(this);
+}
+
+void aiVehicleSpline::SetSpeed(float speed)
+{
+    _curSpeed.set(this, speed);
+}
+
+aiRailSet* aiVehicleSpline::GetRailSet()
+{
+    return _railSet.ptr(this);
+}
+
+aiVehicleInstance * aiVehicleSpline::GetInst() const
+{
+    return _vehicleInstance.get(this);
+}
+
+void aiVehicleSpline::SetMatrix(Matrix34 const& mtx)
+{
+    _matrix.get(this)->Set(mtx);
+}
+
+void aiVehicleSpline::SolveYPositionAndOrientation()
+{
+    hook::Thunk<0x5690C0>::Call<void>(this);
+}
+
+//lua
+void aiVehicleSpline::BindLua(LuaState L) {
+    LuaBinding(L).beginExtendClass<aiVehicleSpline, aiVehicle>("aiVehicleSpline")
+        .addPropertyReadOnly("Instance", &GetInst)
+        .endClass();
+}
+    
--- src/modules/ai/aiVehicleSpline.h
+++ src/modules/ai/aiVehicleSpline.h
@@ -23,71 +23,42 @@ namespace MM2
         static hook::Field<0xD8, Matrix34*> _matrix;
         static hook::Field<0xF4, float> _curSpeed;
     public:
-        aiVehicleSpline(void)                               DONOTCALL;
+        aiVehicleSpline()                               DONOTCALL;
         aiVehicleSpline(const aiVehicleSpline &&)           DONOTCALL;
 
-        void UpdateObstacleMap(void)                        { hook::Thunk<0x568410>::Call<void>(this); }
+        void UpdateObstacleMap();
+        void Position(Vector3& a1) override;
+        float Speed() override;
+        int CurrentRoadIdx(aiPath** a1, const bool* a2, int* a3) override;
+        int CurrentRdVert() override;
 
-        void Position(Vector3 &a1) override                 { hook::Thunk<0x551C40>::Call<void>(this, &a1); }
-        float Speed(void) override                          FORWARD_THUNK;
-        int CurrentRoadIdx(aiPath **a1, const bool *a2, int *a3) override
-                                                            FORWARD_THUNK;
-        int CurrentRdVert(void) override                    FORWARD_THUNK;
+        void Update() override;
+        void Reset() override;
+        int Type() override;
+        Matrix34& GetMatrix() const override;
+        float FrontBumperDistance() override;
+        float BackBumperDistance() override;
+        float LSideDistance() override;
+        float RSideDistance() override;
+        int CurrentLane() override;
+        int CurrentRoadId() override;
+        void DrawId() const override;
 
-        void Update(void) override                          FORWARD_THUNK;
-        void Reset(void) override                           FORWARD_THUNK;
-        int Type(void) override                             FORWARD_THUNK;
-        Matrix34 & GetMatrix(void) override                 { return hook::Thunk<0x551C80>::Call<Matrix34&>(this); }
-        float FrontBumperDistance(void) override            FORWARD_THUNK;
-        float BackBumperDistance(void) override             FORWARD_THUNK;
-        float LSideDistance(void) override                  FORWARD_THUNK;
-        float RSideDistance(void) override                  FORWARD_THUNK;
-        int CurrentLane(void) override                      FORWARD_THUNK;
-        int CurrentRoadId(void) override                    FORWARD_THUNK;
-        void DrawId(void) const override                    FORWARD_THUNK;
-
-        virtual void Impact(int a1)                         FORWARD_THUNK;
-        virtual AudImpact * GetAudImpactPtr(void)           FORWARD_THUNK;
-        virtual void PlayHorn(float a1, float a2)           { hook::Thunk<0x551CA0>::Call<void>(this, a1, a2); }
-        virtual void StopVoice(void)                        { hook::Thunk<0x551CB0>::Call<void>(this); }
+        virtual void Impact(int a1);
+        virtual AudImpact* GetAudImpactPtr();
+        virtual void PlayHorn(float a1, float a2);
+        virtual void StopVoice();
 
         //fields
-        float GetSpeed() const
-        {
-            return _curSpeed.get(this);
-        }
-
-        void SetSpeed(float speed)
-        {
-            _curSpeed.set(this, speed);
-        }
-
-        aiRailSet* GetRailSet()
-        {
-            return _railSet.ptr(this);
-        }
-
-        aiVehicleInstance * GetInst() const 
-        {
-            return _vehicleInstance.get(this);
-        }
-
-        void SetMatrix(Matrix34 const& mtx)
-        {
-            _matrix.get(this)->Set(mtx);
-        }
-
-        void SolveYPositionAndOrientation()
-        {
-            hook::Thunk<0x5690C0>::Call<void>(this);
-        }
+        float GetSpeed() const;
+        void SetSpeed(float speed);
+        aiRailSet* GetRailSet();
+        aiVehicleInstance* GetInst() const;
+        void SetMatrix(Matrix34 const& mtx);
+        void SolveYPositionAndOrientation();
 
         //lua
-        static void BindLua(LuaState L) {
-            LuaBinding(L).beginExtendClass<aiVehicleSpline, aiVehicle>("aiVehicleSpline")
-                .addPropertyReadOnly("VehicleInstance", &GetInst)
-                .endClass();
-        }
+        static void BindLua(LuaState L);
     };
 
     ASSERT_SIZEOF(aiVehicleSpline, 0x18C);
