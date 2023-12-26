ea6d321a0ac61c32a459463a86dd3c17007ed6e4
Dummiesman <aaro4130@gmail.com>
2023-12-25 19:02:15 -0500

    Map out skeleton stuff

--- src/modules/creature/crskeleton.h
+++ src/modules/creature/crskeleton.h
@@ -4,15 +4,31 @@
 namespace MM2
 {
     // Forward declarations
+    struct crBone;
     class crSkeleton;
-    class crSkeletonData;
+    struct crSkeletonData;
+    struct crBoneData;
+    struct crSkeletonData;
 
     // External declarations
 
 
     // Class definitions
+    struct crBone
+    {
+        Vector3 EulerAngles;
+        Matrix34 Matrix;
+        Matrix34* dword_3c;
+    };
 
-    class crSkeleton {
+
+    class crSkeleton 
+    {
+    private:
+        int BoneCount;
+        crBone* Bones;
+        int dword_08;
+        Matrix34* SomeMatrix;
     public:
         AGE_API void Init(crSkeletonData *a1, Matrix34 *a2) { hook::Thunk<0x57C5D0>::Call<void>(this, a1, a2); }
         AGE_API void Update()                               { hook::Thunk<0x57C630>::Call<void>(this); }
@@ -20,8 +36,26 @@ namespace MM2
         AGE_API void Attach(Matrix44 *a1)                   { hook::Thunk<0x57C690>::Call<void>(this, a1); }
     };
 
-    // TODO
-    class crSkeletonData {};
+    struct crBoneData
+    {
+        char Name[64];
+        crBoneData* Next;
+        crBoneData* Child;
+        crBoneData* Parent;
+        Vector3 Offset;
+        int dword_58;
+        int ID;
+        int MirrorBoneID;
+        Vector3 RotMin;
+        Vector3 RotMax;
+    };
+
+
+    struct crSkeletonData
+    {
+        int NumBones;
+        crBoneData* Bones;
+    };
 
     // Lua initialization
 
