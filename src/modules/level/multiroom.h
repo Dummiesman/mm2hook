37a11270465f140c925bfb2baae06019e3a38b1b
Dummiesman <aaro4130@gmail.com>
2023-12-28 02:35:59 -0500

    Map out lvlMultiRoomInstance

--- src/modules/level/multiroom.h
+++ src/modules/level/multiroom.h
@@ -1,16 +1,24 @@
 #pragma once
+#include <modules\level\inst.h>
 
 namespace MM2
 {
-    // Forward declarations
+    struct lvlMultiRoomState
+    {
+        int LastSample;
+        phBound* Bound;
+        Matrix34* Matrix;
+    };
 
-
-    // External declarations
-
-
-    // Class definitions
-
-
-    // Lua initialization
-
+    class lvlMultiRoomInstance : public lvlInstance
+    {
+    private:
+        lvlInstance* m_SrcInstance;
+        lvlMultiRoomState* m_State;
+    public:
+        static void Create(lvlInstance* srcInstance, int room)
+        {
+            hook::StaticThunk<0x467C40>::Call<void>(srcInstance, room);
+        }
+    };
 }
