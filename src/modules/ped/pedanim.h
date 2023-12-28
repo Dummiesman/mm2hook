6b1230b96ebaf7033d85ce43939d7df209ed7da5
Dummiesman <aaro4130@gmail.com>
2023-12-28 02:36:38 -0500

    More pedanim mapping

--- src/modules/ped/pedanim.h
+++ src/modules/ped/pedanim.h
@@ -4,62 +4,77 @@
 namespace MM2
 {
     // Forward declarations
+    struct pedAnimationSequence;
     class pedAnimation;
     class pedAnimationInstance;
 
     // External declarations
     extern class crAnimation;
     extern class crSkeleton;
-    extern class crSkeletonData;
+    extern struct crSkeletonData;
     extern class modModel;
     extern class modShader;
 
     // Class definitions
     
+    struct pedAnimationSequence {
+        const char* Name;
+        uint32_t FrameCount;
+        float FSpeed;
+        float LSpeed;
+        int32_t Direction;
+        crAnimation* pCrAnimation;
+        float FrameBias; // Unused?
+        const char* NextName;
+        uint32_t NextIndex;
+    };
+
     class pedAnimation {
     public:
-        struct Sequence {
-            const char *Name;
-            uint32_t dword4;
-            float float8;
-            float floatC;
-            uint32_t dword10;
-            crAnimation *pCrAnimation;
-            uint32_t dword18;
-            const char *NextName;
-            uint32_t NextIndex;
-        };
-
         int AnimationCount;
-        Sequence *Animations;
+        pedAnimationSequence* Animations;
         Matrix34 matrix348;
         modModel *pModel;
         modShader **ppShaders;
         crSkeleton *pSkeleton;
         crSkeletonData SkeletonData;
-        float *pvoid4C;
-        Vector2 *pvoid50;
-        uint8_t *pvoid54;
-        uint8_t *pvoid58;
-        uint8_t **dword5C;
+        float* RaysHeightMod;
+        Vector2 *RaysBoneSizes;
+        uint8_t *RaysOriginBone;
+        uint8_t *RaysUnknown;
+        uint8_t ** RaysShaderIndices;
         uint32_t *pColors;
         uint32_t SphereIndex;
         int nColorCount;
         uint16_t HashTableIndex;
+    public:
+        modModel* GetModel()                                    { return pModel; }
+        modShader* GetShaders(int variant)                      { return ppShaders[variant]; }
+        crSkeleton* GetSkeleton()                               { return pSkeleton; }
+        pedAnimationSequence* GetSequence(int index)            { return &Animations[index]; }
+        int GetSequenceCount() const                            { return AnimationCount; }
+
+        void DrawSkeleton(int variant, crSkeleton* skeleton)    { hook::Thunk<0x57AB60>::Call<void>(this, variant, skeleton); }
     };
 
     class pedAnimationInstance {
     protected:
-        static hook::Field<0x4, pedAnimation *> _animation;
-        static hook::Field<0x8, int> _variant;
+        void* m_Active; // pedActive*
+        pedAnimation* m_Animation;
+        int m_Variant;
+        __int16 m_Frame;
+        unsigned __int8 m_CurrentState;
+        unsigned __int8 m_NextState;
     public:
-        inline pedAnimation * getAnimation() const {
-            return _animation.get(this);
-        }
+        int GetCurrentState() const         { return m_CurrentState; }
+        int GetNextState() const            { return m_NextState; }
+        pedAnimation * GetAnimation() const { return m_Animation; }
+        int GetVariant() const              { return m_Variant; }
+        int GetCurrentFrame() const         { return m_Frame; }
 
-        inline int getVariant() const {
-            return _variant.get(this);
-        }
+        void SetVariant(int variant)        { m_Variant = variant; }
+        void SetCurrentFrame(int frame)     { m_Frame = frame; }
+        void Start(int nextStateIndex)      { hook::Thunk<0x57B550>::Call<void>(this, nextStateIndex); }
     };
 
     // Lua initialization
