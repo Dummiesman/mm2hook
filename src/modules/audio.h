0b1296efaa0688c4671864d6a509e3e02fe3d342
Dummiesman <aaro4130@gmail.com>
2023-12-29 14:29:07 -0500

    Update audio module

--- src/modules/audio.h
+++ src/modules/audio.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <mm2_common.h>
 
+#include "audio\audmanagerbase.h"
 #include "audio\sound.h"
 #include "audio\speech.h"
 #include "audio\aud3dobject.h"
@@ -9,6 +10,14 @@
 #include "audio\aud3dambientobject_wrapper.h"
 #include "audio\audimpactdata.h"
 #include "audio\audimpact.h"
+
+#include "directmusic\dmusicobject.h"
+#include "directmusic\dmusicmanager.h"
+
+#include "mmaudio\cnrspeech.h"
+#include "mmaudio\racespeech.h"
+#include "mmaudio\mmaudiomanager.h"
+#include "mmaudio\mmdmusicmanager.h"
 
 namespace MM2
 {
