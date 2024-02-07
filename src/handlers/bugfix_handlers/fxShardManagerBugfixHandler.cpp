94de985547ae74df6af59ab800f156130b70730a
Dummiesman <aaro4130@gmail.com>
2024-02-07 04:52:44 -0500

    Remove fxShardManager instance array write

--- src/handlers/bugfix_handlers/fxShardManagerBugfixHandler.cpp
+++ src/handlers/bugfix_handlers/fxShardManagerBugfixHandler.cpp
@@ -8,6 +8,11 @@ using namespace MM2;
 
 void fxShardManagerBugfixHandler::Install()
 {
+    // The fxShardManager::Instances array is an unreferenced MM1 holdover that causes crashes with 41+vehicles
+    InstallPatch("Remove fxShardManager instance array.", { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 }, {
+        0x45FE8B,
+    });
+
     InstallCallback("fxShardManager::Drraw", "Fix crashes with cars with low material count.",
         &fxShardManager::Draw, {
             cb::jmp(0x4602D0)
