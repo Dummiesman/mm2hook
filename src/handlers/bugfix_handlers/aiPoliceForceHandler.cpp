2f3713ffb83fd253d19a4a3d37b46b896e15456c
Dummiesman <aaro4130@gmail.com>
2024-02-08 12:20:52 -0500

    Rewrite and expand on aiPoliceForce

--- src/handlers/bugfix_handlers/aiPoliceForceHandler.cpp
+++ src/handlers/bugfix_handlers/aiPoliceForceHandler.cpp
@@ -6,6 +6,26 @@ using namespace MM2;
     aiPoliceForceHandler
 */
 
+void* aiPoliceForceHandler::AllocationSizeHook(unsigned int size)
+{
+    return hook::StaticThunk<0x577360>::Call<void*>(sizeof(aiPoliceForce));
+}
+
+BOOL aiPoliceForceHandler::UnRegisterCop(MM2::vehCar* copCar, MM2::vehCar* perpCar)
+{
+    return reinterpret_cast<aiPoliceForce*>(this)->UnRegisterCop(copCar, perpCar);
+}
+
+BOOL aiPoliceForceHandler::RegisterPerp(MM2::vehCar* copCar, MM2::vehCar* perpCar)
+{
+    return reinterpret_cast<aiPoliceForce*>(this)->RegisterPerp(copCar, perpCar);
+}
+
+int aiPoliceForceHandler::State(MM2::vehCar* copCar, MM2::vehCar* perpCar, float dist)
+{
+    return reinterpret_cast<aiPoliceForce*>(this)->State(copCar, perpCar, dist);
+}
+
 void aiPoliceForceHandler::Reset(void) {
     // reset number of cops pursuing player
     // fixes incorrect music bug
@@ -18,6 +38,31 @@ void aiPoliceForceHandler::Install() {
         &Reset, {
             cb::call(0x536AAE),
             cb::call(0x550ECA),
+        }
+    );
+
+    InstallCallback("aiPoliceForce::State", "Use new State function.",
+        &State, {
+            cb::call(0x53DDE8),
+        }
+    );
+
+    InstallCallback("aiPoliceForce::UnRegisterCop", "Use new UnRegisterCop function.",
+        &UnRegisterCop, {
+            cb::call(0x53F1B7),
+        }
+    );
+
+    InstallCallback("aiPoliceForce::RegisterPerp", "Use new RegisterPerp function.",
+        &RegisterPerp, {
+            cb::call(0x53E06E),
+            cb::call(0x53E1C7),
+        }
+    );
+
+    InstallCallback("new aiPoliceForce()", "Use new size.",
+        &AllocationSizeHook, {
+            cb::call(0x535C91),
         }
     );
 }
--- src/modules/ai/aiPoliceForce.h
+++ src/modules/ai/aiPoliceForce.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <mm2_common.h>
 
 namespace MM2
 {
@@ -9,13 +10,12 @@ namespace MM2
     extern class vehCar;
 
     // Class definitions
-
     class aiPoliceForce {
     private:
-        static const int NUM_COPS = 3;
-        static const int NUM_TARGETS = 3;
+        static const int NUM_COPS = 16; // Default 3
+        static const int NUM_TARGETS = 16; // Default 3
     private:
-        Timer timer;
+        Timer timer; // Tracks the last time a cop started giving chase
         short NumPerps;
         short NumChasers[NUM_TARGETS];
         vehCar* PerpCars[NUM_TARGETS];
@@ -24,56 +24,17 @@ namespace MM2
         aiPoliceForce(void)                                       DONOTCALL;
         aiPoliceForce(const aiPoliceForce &&)                     DONOTCALL;
 
-        AGE_API int UnRegisterCop(vehCar *cop, vehCar *perp)      { return hook::Thunk<0x551040>::Call<int>(this, cop, perp); }
-        AGE_API int RegisterPerp(vehCar *cop, vehCar *perp)       { return hook::Thunk<0x550FA0>::Call<int>(this, cop, perp); }
-        AGE_API int Find(vehCar *cop, vehCar *perp) const         { return hook::Thunk<0x550F40>::Call<int>(this, cop, perp); }
-        AGE_API int State(vehCar *cop, vehCar *perp, float a3 ) const
-                                                                  { return hook::Thunk<0x551120>::Call<int>(this, cop, perp, a3); }
-        AGE_API void Reset()                                      { hook::Thunk<0x550EF0>::Call<void>(this); }
-
-        int GetNumChasers(vehCar* perp) const
-        {
-            for (int i = 0; i < NUM_TARGETS; i++)
-            {
-                if (PerpCars[i] == perp)
-                {
-                    return this->NumChasers[i];
-                }
-            }
-            return 0;
-        }
-
-        vehCar* GetChaser(vehCar* perp, int chaserIndex) const
-        {
-            if (chaserIndex < 0 || chaserIndex >= NUM_COPS)
-                return nullptr;
-            
-            for (int i = 0; i < NUM_TARGETS; i++)
-            {
-                if (PerpCars[i] == perp && chaserIndex < NumChasers[i])
-                {
-                    return this->CopCars[i][chaserIndex];
-                }
-            }
-            return nullptr;
-        }
-
-        int GetNumPerps() const
-        {
-            return this->NumPerps;
-        }
-        static void BindLua(LuaState L) {
-            LuaBinding(L).beginClass<aiPoliceForce>("aiPoliceForce")
-                .addPropertyReadOnly("NumPerps", &GetNumPerps)
-                .addFunction("GetNumChasers", &GetNumChasers)
-                .addFunction("GetChaser", &GetChaser)
-                .addFunction("UnRegisterCop", &UnRegisterCop)
-                .addFunction("RegisterPerp", &RegisterPerp)
-                .addFunction("Find", &Find)
-                .addFunction("State", &State)
-                .addFunction("Reset", &Reset)
-                .endClass();
-        }
+        int GetNumChasers(vehCar* perp) const;
+        vehCar* GetPerp(int perpIndex) const;
+        vehCar* GetChaser(vehCar* perp, int chaserIndex) const;
+        int GetNumPerps() const;
+        BOOL UnRegisterCop(vehCar* cop, vehCar* perp);
+        BOOL RegisterPerp(vehCar* cop, vehCar* perp);
+        int Find(vehCar* cop, vehCar* perp);
+        int State(vehCar* cop, vehCar* perp, float distance);
+        void Reset();
+        
+        static void BindLua(LuaState L);
     };
 
     // Lua initialization
