0ebfddec13675e54ddffe14f6632c584e790a745
Dummiesman <aaro4130@gmail.com>
2024-07-28 13:24:13 -0400

    no message

--- src/handlers/feature_handlers/mmGameMultiHandler.h
+++ src/handlers/feature_handlers/mmGameMultiHandler.h
@@ -5,7 +5,6 @@
 
 class mmGameMultiHandler {
 public:
-    int Init();
     void GameMessageHook(MM2::NET_RCXHEAD* rcxhead);
     void HandleSystemMessage(DPMSG_GENERIC* msg);
     static void Install();
