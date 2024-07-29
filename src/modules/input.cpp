14911f0ba2aa2a4167a5efab84c1eae1001f97b4
Dummiesman <aaro4130@gmail.com>
2024-07-29 02:57:26 -0400

    Fix address for dX

--- src/modules/input.cpp
+++ src/modules/input.cpp
@@ -9,7 +9,7 @@ declfield(ioMouse::InvHeight)       (0x6A38D4);
 
 declfield(ioMouse::m_X)            (0x6A38DC);
 declfield(ioMouse::m_Y)            (0x6A38D8);
-declfield(ioMouse::m_dX)            (0x6A38CC);
+declfield(ioMouse::m_dX)            (0x6A38B0);
 declfield(ioMouse::m_dY)            (0x6A38F0);
 declfield(ioMouse::m_dZ)            (0x6A38E8);
 declfield(ioMouse::m_LastButtons)   (0x6A38E4);
